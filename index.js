718323024bb958bf971a591a8a8b24ae25a4dbeb
Nicolas Simon <nicolas1.simon@epita.fr>
2017-11-29 16:30:52 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,96 @@
+const request = require('request-promise-native').defaults({jar: true});
+
+module.exports = class addTestFlightUser {
+  constructor(itcLogin, itcPassword, appId) {
+    this.loggedIn = false;
+    this.itcLogin = itcLogin;
+    this.itcPassword = itcPassword;
+    this.appId = appId;
+    this.urlITCBase = 'https://itunesconnect.apple.com';
+  }
+
+  async getServiceKey() {
+    const url = this.urlITCBase + '/itc/static-resources/controllers/login_cntrl.js';
+    const content = await request(url);
+    const key = content.match(/itcServiceKey = '(.*)'/g)[0].split('\'')[1];
+    return key;
+  }
+
+  async login() {
+    if (this.loggedIn)
+      return;
+
+    const serviceKey = await this.getServiceKey();
+    const data = {
+      accountName: this.itcLogin,
+      password: this.itcPassword,
+      rememberMe: 'false'
+    };
+    const headers = {
+      'Content-Type': 'application/json',
+      'X-Apple-Widget-Key': await this.getServiceKey()
+    };
+    const loginUrl = 'https://idmsa.apple.com/appleauth/auth/signin';
+
+    const resp = await request.post({
+      url: loginUrl,
+      headers,
+      json: data
+    });
+
+    if (!this.contentProviderId)
+      this.contentProviderId = await this.getFirstContentProviderId();
+
+    this.urlApp = this.urlITCBase + '/testflight/v2/providers' +
+      `/${this.contentProviderId}/apps/${this.appId}`;
+
+    //if (!this.groupId)
+    //  this.groupId = await this.getDefaultExternalGroupId()
+
+    this.loggedIn = true;
+  }
+
+  async getFirstContentProviderId() {
+    const content = await request(this.urlITCBase +
+      '/WebObjects/iTunesConnect.woa/ra/user/detail', {json: true});
+    const account = content.data.associatedAccounts[0];
+    return account.contentProvider.contentProviderId;
+  }
+
+  /*
+  async getDefaultExternalGroupId() {
+    const content = await request(this.urlApp + '/groups', {json: true});
+
+    for (let group of content.data) {
+      if (group.name === this.groupName || group.isDefaultExternalGroup)
+        return group.id;
+    }
+  }
+  */
+
+  async addTester(email, firstName, lastName) {
+    await this.login();
+    let params = {email, firstName, lastName};
+    const content = await request.post({
+      url: this.urlApp + '/testers',
+      headers: {'Content-Type': 'application/json'},
+      json: params
+    });
+    const testerId = content.data.id;
+
+    return;
+    /*
+    //const groupId = this.groupId;
+    params = {testerId, groupId};
+    const url = this.urlApp + `/groups/${groupId}/testers/${testerId}`;
+    console.log(url);
+    await request.put({
+      url,
+      //url: this.urlApp + '/groups/'+ groupId + '/testers/' + testerId,
+      headers: {'Content-Type': 'application/json'},
+      json: params
+    });
+    */
+  }
+
+}
